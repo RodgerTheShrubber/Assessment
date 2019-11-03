413067f1ca322135545e191ccd17437ce2b27c8c
Scott Brumley <tbrumley96@gmail.com>
2019-11-03 13:51:44 -0800

    Add a new react control that will handle swapping language from english to pig latin

--- src/controls/website/Website.tsx
+++ src/controls/website/Website.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { englishStrings, AppStrings } from '../../data'
+import { LanguageSelector } from '..'
+import { IDropdownOption } from 'office-ui-fabric-react/lib/Dropdown';
 
 export interface WebsiteProps {
 }
@@ -16,8 +18,17 @@ export class Website extends React.PureComponent<WebsiteProps, WebsiteState> {
     }
   }
 
-  public render(): JSX.Element {
-    return <div>{englishStrings.heading}</div>
+  private onLanguageChange = (languageOption: IDropdownOption) => {
+    // When the language dropdown changes we swap out the app strings object with the new language. If we can't get a language object for that option
+    // fall back to english strings.
+    this.setState({ appStrings: languageOption.data.languageOption || englishStrings})
   }
+
+  public render(): JSX.Element {
+    return (<div>
+      <LanguageSelector onLanguageChange={this.onLanguageChange}/>
+      {this.state.appStrings.heading}
+      </div>
+  )};
 }
 
