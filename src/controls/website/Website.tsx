8cef36056d5f6f6acc6cf5896abb25b669be9d5c
Scott Brumley <tbrumley96@gmail.com>
2019-11-03 13:19:26 -0800

    Add typing to the strings we will be using

--- src/controls/website/Website.tsx
+++ src/controls/website/Website.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
+import { englishStrings, AppStrings } from '../../data'
 
 export interface WebsiteProps {
-
 }
 
 export interface WebsiteState {
-  
+  appStrings: AppStrings;
 }
 
 export class Website extends React.PureComponent<WebsiteProps, WebsiteState> {
   public constructor(props: WebsiteProps) {
     super(props);
+    this.state = {
+      appStrings: englishStrings
+    }
   }
 
   public render(): JSX.Element {
-    return <div>Hellow World</div>
+    return <div>{englishStrings.heading}</div>
   }
 }
 
---
+++ src/data/appStrings.ts
@@ -0,0 +1,29 @@
+import { string } from "prop-types"
+
+export interface AppStrings {
+        heading: string, 
+        description: string,
+        snippets: string[],
+        locations: string[],
+        "video-embed": string,
+        quote: quote,
+        gallery: galleryItem[],
+        "episode-list": episodeDetails[]
+    }
+
+export interface quote {
+    text: string,
+    author: string
+}
+
+export interface galleryItem {
+    src: string,
+    text: string,
+    reference?: string
+}
+
+export interface episodeDetails {
+    season: number,
+    name: string,
+    rating: number
+}
